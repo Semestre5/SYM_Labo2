b0e91331e4b1197f85af6b98b212c5595de89efa
Axel Vallon <axel.vallon123@gmail.com>
2021-11-08 19:40:55 +0100

    ajout source ?

---
+++ Labo2/app/src/main/java/ch/heigvd/sym/labo2/CommunicationEventListener.kt
@@ -0,0 +1,5 @@
+package ch.heigvd.sym.labo2
+
+interface CommunicationEventListener {
+    fun handleServerResponse(response :String)
+}
---
+++ Labo2/app/src/main/java/ch/heigvd/sym/labo2/SymComManager.kt
@@ -0,0 +1,11 @@
+package ch.heigvd.sym.labo2
+
+import ch.heigvd.sym.labo2.CommunicationEventListener
+
+class SymComManager(var communicationEventListener: CommunicationEventListener? = null) {
+
+    fun sendRequest(url: String, request: String) {
+		//TODO to implement
+    }
+
+}
