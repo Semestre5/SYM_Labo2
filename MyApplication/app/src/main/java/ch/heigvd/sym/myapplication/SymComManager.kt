eeb07aa932e17f50c265cb76e9b42dbba142f827
Axel Vallon <axel.vallon123@gmail.com>
2021-11-09 15:42:59 +0100

    step 1 - ok

--- MyApplication/app/src/main/java/ch/heigvd/sym/myapplication/SymComManager.kt
+++ MyApplication/app/src/main/java/ch/heigvd/sym/myapplication/SymComManager.kt
@@ -6,36 +6,37 @@ import java.io.UTFDataFormatException
 import java.net.HttpURLConnection
 import java.net.URL
 import java.nio.charset.StandardCharsets
+import kotlin.concurrent.thread
 
 class SymComManager(var communicationEventListener: CommunicationEventListener? = null) {
     fun sendRequest(url: String, request: String) {
-        val connection = URL(url).openConnection() as HttpURLConnection
+            thread() {
+                val connection = URL(url).openConnection() as HttpURLConnection
+                val requestSerialized: ByteArray = request.toByteArray(StandardCharsets.UTF_8);
+                connection.requestMethod = "POST";
+                connection.setRequestProperty("Content-Type", "text/plain");
+                // connection.setRequestProperty("Accept-Charset", "UTF-8");
+                connection.doOutput = true;
 
-        val requestSerialized : ByteArray = request.toByteArray(StandardCharsets.UTF_8);
-        connection.requestMethod = "POST";
-        connection.setRequestProperty("Content-Type", "text/plain");
-        try {
-            connection.outputStream.write(requestSerialized);
-            connection.outputStream.close();
-        } catch (exception: Exception) {
-            exception.printStackTrace()
-            return
-        } /*finally {
-            connection.disconnect()
-        }*/
+                try {
+                    connection.outputStream.write(requestSerialized);
+                    connection.outputStream.close();
+                } catch (exception: Exception) {
+                    exception.printStackTrace()
+                }
 
-        try {
-            val br = BufferedReader(
-                InputStreamReader(connection.inputStream)
-            )
-            val response : String = br.readText()
-            br.close()
-        }
-        catch (exception: Exception) {
-            exception.printStackTrace()
-            return
-        }/* finally {
-            connection.disconnect()
-        }*/
-        }
+                try {
+                    val br = BufferedReader(
+                        InputStreamReader(connection.inputStream)
+                    )
+                    val response: String = br.readText()
+                    communicationEventListener?.handleServerResponse(response)
+                    br.close()
+                } catch (exception: Exception) {
+                    exception.printStackTrace()
+                } finally {
+                    connection.disconnect()
+                }
+            }
+    }
 }
