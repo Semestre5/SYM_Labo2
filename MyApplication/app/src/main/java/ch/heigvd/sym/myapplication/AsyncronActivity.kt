15a880d09a2d79927da66fd1f482eb24166b23a9
Axel Vallon <axel.vallon123@gmail.com>
2021-11-09 13:09:18 +0100

    basic app ok

---
+++ MyApplication/app/src/main/java/ch/heigvd/sym/myapplication/AsyncronActivity.kt
@@ -0,0 +1,11 @@
+package ch.heigvd.sym.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AsyncronActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_asyncron)
+    }
+}
--- MyApplication/app/src/main/java/ch/heigvd/sym/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/ch/heigvd/sym/myapplication/MainActivity.kt
@@ -1,11 +1,40 @@
 package ch.heigvd.sym.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ch.heigvd.sym.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root);
+
+        binding.buttonAsync.setOnClickListener {
+            val intent = Intent(this@MainActivity, AsyncronActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonDiff.setOnClickListener {
+            val intent = Intent(this@MainActivity, DeferedActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonSerialization.setOnClickListener {
+            val intent = Intent(this@MainActivity, SerializationActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonCompressed.setOnClickListener {
+            val intent = Intent(this@MainActivity, CompressActivity::class.java)
+            startActivity(intent)
+        }
+        binding.buttonGraphql.setOnClickListener {
+            val intent = Intent(this@MainActivity, GraphActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
