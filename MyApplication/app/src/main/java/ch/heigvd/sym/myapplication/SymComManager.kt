3c97c477e564466873ae14a8f8e49a28bba4cbdf
Axel Vallon <axel.vallon123@gmail.com>
2021-11-09 14:08:57 +0100

    ajout read

--- MyApplication/app/src/main/java/ch/heigvd/sym/myapplication/SymComManager.kt
+++ MyApplication/app/src/main/java/ch/heigvd/sym/myapplication/SymComManager.kt
@@ -1,8 +1,41 @@
 package ch.heigvd.sym.myapplication
 
-class SymComManager(var communicationEventListener: CommunicationEventListener? = null) {
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.UTFDataFormatException
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.charset.StandardCharsets
 
+class SymComManager(var communicationEventListener: CommunicationEventListener? = null) {
     fun sendRequest(url: String, request: String) {
-		//TODO to implement
-    }
+        val connection = URL(url).openConnection() as HttpURLConnection
+
+        val requestSerialized : ByteArray = request.toByteArray(StandardCharsets.UTF_8);
+        connection.requestMethod = "POST";
+        connection.setRequestProperty("Content-Type", "text/plain");
+        try {
+            connection.outputStream.write(requestSerialized);
+            connection.outputStream.close();
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+            return
+        } /*finally {
+            connection.disconnect()
+        }*/
+
+        try {
+            val br = BufferedReader(
+                InputStreamReader(connection.inputStream)
+            )
+            val response : String = br.readText()
+            br.close()
+        }
+        catch (exception: Exception) {
+            exception.printStackTrace()
+            return
+        }/* finally {
+            connection.disconnect()
+        }*/
+        }
 }
