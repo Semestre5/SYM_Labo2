e5e6285d42d758d34acfaa4af39fbe5292a45a7a
Rotho1998 <rob.gaudin@hotmail.com>
2021-11-10 15:04:31 +0100

    Début

--- MyApplication/app/src/main/java/ch/heigvd/sym/myapplication/CompressActivity.kt
+++ MyApplication/app/src/main/java/ch/heigvd/sym/myapplication/CompressActivity.kt
@@ -2,10 +2,23 @@ package ch.heigvd.sym.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ch.heigvd.sym.myapplication.databinding.ActivityCompressBinding
 
-class CompressActivity : AppCompatActivity() {
+class CompressActivity : CommunicationEventListener, AppCompatActivity() {
+    private lateinit var binding: ActivityCompressBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_compress)
+        binding = ActivityCompressBinding.inflate(layoutInflater)
+        setContentView(binding.root);
+        val symComManager = SymComManager(this)
+
+        binding.sendButton.setOnClickListener {
+            symComManager.sendRequest("http://mobile.iict.ch/api/txt", binding.userInput.text.toString(), compressed = true)
+        }
+    }
+
+    override fun handleServerResponse(response: String) {
+        binding.textAnswer.text = response
     }
 }
